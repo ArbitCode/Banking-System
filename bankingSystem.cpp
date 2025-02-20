071dcb5cddc2c23953346057f92c8bdf5e07fb77
Raja Sharma <raja@ibm.com>
2025-02-20 11:52:44 +0530

    menu update

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -1,109 +1,124 @@
 #include "bank.hpp"
+#include<limits>
 
 using namespace std;
 
-template <typename T>
-bool isInputValid(T &data)
+enum class Ops
 {
-  if (cin >> data)
-    return true;
-  return false;
+  OPEN_ACCOUNT = 1,
+  BALANCE_ENQUIRY,
+  DEPOSIT,
+  WITHDRAW,
+  CLOSE_ACCOUNT,
+  SHOW_ALL_ACCOUNTS,
+  QUIT
+};
+
+template <typename T>
+void getInput(T &data)
+{
+  short count = 3;
+  while (!(cin >> data)){
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (count){
+      cout << "\n\tERROR: Invalid Input! left chance: " << count;
+      cout << "\nPlease Enter Again: ";
+      count--;
+    }else{
+      cout << "\n\tERROR: Exit!\n";
+      exit(0);
+    }
+  }
 }
 
 int main()
 {
   Bank b;
   Account acc;
+  Ops ops;
 
-  int choice;
-  std::string fname, lname;
-  long accountNumber;
-  float balance;
-  float amount;
-
-  cout << "***Banking System***" << endl;
+  int choice = -1;
+  std::string fname = "";
+  std::string lname = "";
+  long accountNumber = -1;
+  float balance = 0;
+  float amount = 0;
+  cout << "\n----------------------------";
+  cout << "\n\tBanking System" << endl;
+  cout << "\n----------------------------";
   do
   {
     cout << "\n\tSelect one option below ";
-    cout << "\n\t1 Open an Account";
-    cout << "\n\t2 Balance Enquiry";
-    cout << "\n\t3 Deposit";
-    cout << "\n\t4 Withdraw";
-    cout << "\n\t5 Close an Account";
-    cout << "\n\t6 Show All Accounts";
-    cout << "\n\t7 Quit";
-    cout << "\nEnter your choice: ";
+    cout << "\n\t1. Open an Account";
+    cout << "\n\t2. Balance Enquiry";
+    cout << "\n\t3. Deposit";
+    cout << "\n\t4. Withdraw";
+    cout << "\n\t5. Close an Account";
+    cout << "\n\t6. Show All Accounts";
+    cout << "\n\t7. Quit";
+    cout << "\n----------------------------";
+    cout << "\nEnter your choice(1-7): ";
 
-    if (!isInputValid(choice))
-      goto OUTSIDE;
+    getInput(choice);
+    ops = static_cast<Ops>(choice);
 
-    switch (choice)
-    {
-    case 1:
+    switch (ops){
+    case Ops::OPEN_ACCOUNT:
       cout << "Enter First Name: ";
-      if (!isInputValid(fname))
-        goto OUTSIDE;
+      getInput(fname);
       cout << "Enter Last Name: ";
-      if (!isInputValid(lname))
-        goto OUTSIDE;
+      getInput(lname);
       cout << "Enter initial Balance: ";
-      if (!isInputValid(balance))
-        goto OUTSIDE;
+      getInput(balance);
       acc = b.openAccount(fname, lname, balance);
       cout << endl
            << "Congratulation Account is Created" << endl;
       cout << acc;
       break;
-    case 2:
+    case Ops::BALANCE_ENQUIRY:
       cout << "Enter Account Number: ";
-      if (!isInputValid(accountNumber))
-        goto OUTSIDE;
+      getInput(accountNumber);
       acc = b.balanceEnquiry(accountNumber);
       cout << endl
            << "Your Account Details" << endl;
       cout << acc;
       break;
-    case 3:
+    case Ops::DEPOSIT:
       cout << "Enter Account Number: ";
-      if (!isInputValid(accountNumber))
-        goto OUTSIDE;
+      getInput(accountNumber);
       cout << "Enter Balance: ";
-      if (!isInputValid(amount))
-        goto OUTSIDE;
+      getInput(amount);
       acc = b.deposit(accountNumber, amount);
       cout << endl
            << "Amout is Deposited" << endl;
       cout << acc;
       break;
-    case 4:
+    case Ops::WITHDRAW:
       cout << "Enter Account Number: ";
-      if (!isInputValid(accountNumber))
-        goto OUTSIDE;
+      getInput(accountNumber);
       cout << "Enter Balance: ";
-      if (!isInputValid(amount))
-        goto OUTSIDE;
+      getInput(amount);
       acc = b.withdraw(accountNumber, amount);
       cout << endl
            << "Amount withdrawn" << endl;
       cout << acc;
       break;
-    case 5:
+    case Ops::CLOSE_ACCOUNT:
       cout << "Enter Account Number: ";
-      if (!isInputValid(accountNumber))
-        goto OUTSIDE;
+      getInput(accountNumber);
       b.closeAccount(accountNumber);
       break;
-    case 6:
+    case Ops::SHOW_ALL_ACCOUNTS:
       b.showAllAccounts();
       break;
-    case 7:
+    case Ops::QUIT:
+      cout << "Thank you!";
       break;
     default:
-      cout << "\nEnter correct choice";
-      exit(0);
+      cout << "\nEnter correct choice: ";
     }
-  } while (choice != 7);
-  OUTSIDE:
-    cout << "Invalid Input: Please try again\n";
+  } while (ops != Ops::QUIT);
+
   return 0;
 }
